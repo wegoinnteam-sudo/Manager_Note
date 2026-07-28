47a1a71ba67dd263b090b2db1c640856b2e45c0d
wegoinnteam-sudo <wegoinnteam@gmail.com>
2026-07-28 08:50:47 +0000

    Redirect legacy Pages URL to Worker app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+const LEGACY_PAGES_HOST = "manager-note.pages.dev";
+const APP_ORIGIN = "https://team-handoff-notes.wegoinnteam.workers.dev";
+
+if (window.location.hostname === LEGACY_PAGES_HOST) {
+  window.location.replace(`${APP_ORIGIN}${window.location.pathname}${window.location.search}${window.location.hash}`);
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
